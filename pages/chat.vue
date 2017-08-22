c40fcdf8a7e42e1783db96c58d79ca1374168761
karlcode <karl.l1@hotmail.com>
2017-08-22 21:57:42 +1000

    Made chat feature functional through refactoring firebase initialisation

--- pages/chat.vue
+++ pages/chat.vue
@@ -1,6 +1,6 @@
 <template>
 <v-layout column >
-<span>{{initName}}</span>
+<!--<span>{{initName}}</span>
       <form id="form" v-on:submit.prevent="initName">
       <input type="text" v-model.lazy="initName" placeholder="Your name">
       <input type="submit" value="Create Profile">
@@ -11,44 +11,40 @@
       <form id="form" v-on:submit.prevent="addUser">
       <input type="text" v-model="newUser.name" placeholder="Message">
       <input type="submit" value="Add Message">
-      </form>
+      </form>-->
+      <input v-model="newMessage" @keyup.enter="addMessage" placeholder="Add message" />
+      <h1>YOLO</h1>
+      
 </v-layout>
+
 </template>
 
 <script>
   import Vue from 'vue'
-  var VueFire = require('vuefire')
-  Vue.use(VueFire)
-  import * as firebase from "firebase";
-  var config = {
-    apiKey: "AIzaSyCdqIU_fTgJtMnAOhwkhA3qekjeoUynf3k",
-    authDomain: "dating-prototype.firebaseapp.com",
-    databaseURL: "https://dating-prototype.firebaseio.com",
-    projectId: "dating-prototype",
-    storageBucket: "dating-prototype.appspot.com",
-    messagingSenderId: "34141672867"
-  };
-  firebase.initializeApp(config)
+  import firebase from "firebase";
+  const firebaseApp = firebase.initializeApp({
+                apiKey: "AIzaSyCdqIU_fTgJtMnAOhwkhA3qekjeoUynf3k",
+                authDomain: "dating-prototype.firebaseapp.com",
+                databaseURL: "https://dating-prototype.firebaseio.com",
+                projectId: "dating-prototype",
+                storageBucket: "dating-prototype.appspot.com",
+                messagingSenderId: "34141672867"})
+
+  const db = firebaseApp.database()
+  const messagesRef = db.ref('messages')
   export default {
     data: () => ({
-      newUser: {
-        name: '',
-        message: ''
-      },
-      initName: ''
+        newMessage: ''
     }),
-    firebase: {
-    users: firebase.database().ref('users')
-    },
     methods: {
-       addUser: function () {
-        var user = this.initName
-        usersRef.push(this.newUser)
-        this.newUser.name = ''
-      },
-      initName: function (){
-        this.initName = ''
-      }
+        addMessage (){
+            if (this.newMessage.trim()) {
+            messagesRef.push({
+            text: this.newMessage
+          })
+          this.newMessage = ''
+        }
     }
+  }
   }
 </script>
