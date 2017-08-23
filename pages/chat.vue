7066ce0700b283b326600dcdfa01a09c3a4cda53
karlcode <karl.l1@hotmail.com>
2017-08-23 18:55:23 +1000

    Solved multiple Firebase instances error

--- pages/chat.vue
+++ pages/chat.vue
@@ -12,8 +12,9 @@
       <input type="text" v-model="newUser.name" placeholder="Message">
       <input type="submit" value="Add Message">
       </form>-->
-      <input v-model="newMessage" @keyup.enter="addMessage" placeholder="Add message" />
+      
       <h1>YOLO</h1>
+      <input v-model="newMessage" @keyup.enter="addMessage" placeholder="Add message" />
       
 </v-layout>
 
@@ -22,24 +23,30 @@
 <script>
   import Vue from 'vue'
   import firebase from "firebase";
-  const firebaseApp = firebase.initializeApp({
-                apiKey: "AIzaSyCdqIU_fTgJtMnAOhwkhA3qekjeoUynf3k",
-                authDomain: "dating-prototype.firebaseapp.com",
-                databaseURL: "https://dating-prototype.firebaseio.com",
-                projectId: "dating-prototype",
-                storageBucket: "dating-prototype.appspot.com",
-                messagingSenderId: "34141672867"})
+  let config = {
+        apiKey: "AIzaSyCdqIU_fTgJtMnAOhwkhA3qekjeoUynf3k",
+        authDomain: "dating-prototype.firebaseapp.com",
+        databaseURL: "https://dating-prototype.firebaseio.com",
+        projectId: "dating-prototype",
+        storageBucket: "dating-prototype.appspot.com",
+        messagingSenderId: "34141672867"
+}
 
-  const db = firebaseApp.database()
-  const messagesRef = db.ref('messages')
+
+  //const messagesRef = db.ref('messages')
   export default {
+    beforeCreate: () => {
+    if (!firebase.apps.length) {
+    var Firebase = firebase.initializeApp(config)
+    }
+    },
     data: () => ({
         newMessage: ''
     }),
     methods: {
         addMessage (){
             if (this.newMessage.trim()) {
-            messagesRef.push({
+            this.Firebase.ref('messages').push({
             text: this.newMessage
           })
           this.newMessage = ''
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import * as firebase from "firebase";
-const axios = require('axios')
 
-Vue.use(axios)
+let config = {
+    apiKey: "AIzaSyCdqIU_fTgJtMnAOhwkhA3qekjeoUynf3k",
+    authDomain: "dating-prototype.firebaseapp.com",
+    databaseURL: "https://dating-prototype.firebaseio.com",
+    projectId: "dating-prototype",
+    storageBucket: "dating-prototype.appspot.com",
+    messagingSenderId: "34141672867"
+}
+firebase.initializeApp(config)
