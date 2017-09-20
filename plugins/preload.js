3d081b1d60bb582b38ccf239bbf00590c32befe7
karlcode <karl.l1@hotmail.com>
2017-09-20 18:16:08 +1000

    Added client side user retrieve from localStorage

--- plugins/preload.js
+++ plugins/preload.js
@@ -5,6 +5,13 @@ import {config} from '../plugins/firebase'
 if (!firebase.apps.length) {
     firebase.initializeApp(config)
  }
- 
+
+if (process.BROWSER_BUILD) {
+const authUser = Object.keys(window.localStorage)
+ .filter(item => item.startsWith('firebase:authUser'))[0]
+ const user = authUser ? JSON.parse(localStorage.getItem(authUser)) : undefined;
+ //store.commit('setUser' , user)
+}
+
 store.dispatch('retrieveUser')
 
