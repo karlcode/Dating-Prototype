1e4d9cd7f877e4572d1714d7d2f8aabed2cb3a70
karlcode <karl.l1@hotmail.com>
2017-09-25 21:53:13 +1000

    Deleted test cards on inspire page

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -20,7 +20,7 @@
             :key="user.key"
             xs4
           >
-            <v-card >
+            <v-card>
               <a :href=" user.photoURL ">
               <v-card-media
                 :src="user.photoURL"
@@ -46,43 +46,7 @@
           
         </v-layout>
       </v-container>
-        <v-container fluid grid-list-md class="grey lighten-4">
         
-        <v-layout row wrap>
-          <v-flex
-            v-bind="{ [`xs${card.flex}`]: true }"
-            v-for="card in cards"
-            :key="card.title"
-            
-          >
-          
-            <v-card >
-              <a :href=" card.src ">
-              <v-card-media
-                :src="card.src"
-                height="200px"
-              >
-              </v-card-media>
-              </a>
-              <v-card-title primary-title>
-              <div>
-                <span class="headline" v-text="card.title"></span>
-              </div>
-              <v-card-actions class="white">
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>favorite</v-icon>
-                </v-btn>
-                <span v-text="'Age: ' + card.age"></span>
-              </v-card-actions>
-              </v-card-title>
-            
-            </v-card>
-          
-          </v-flex>
-          
-        </v-layout>
-      </v-container>
       </v-flex>
     </v-layout>
 </template>
@@ -105,22 +69,6 @@
     }),
     firebase: {
         users: firebase.database().ref('user/')
-    },
-    mounted() {
-      axios.get('https://randomuser.me/api/?results=50&gender=male')
-      .then(response =>  {
-        var result = response.data.results
-        console.log(result)
-        for(var i=0;i<result.length;i++){
-          this.cards.push({
-            title: result[i].name.first.charAt(0).toUpperCase()+result[i].name.first.slice(1)  + " " + result[i].name.last.charAt(0).toUpperCase()+result[i].name.last.slice(1) ,
-            src: result[i].picture.large,
-            age: Math.floor((Math.random() * 90) + 20),
-            flex: 4
-          }  
-          )
-        }
-      })
     }
   }
 </script>
