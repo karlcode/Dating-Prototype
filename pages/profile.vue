7f39e7d70fd2c50458832f5ce094917ac6adac5e
karlcode <karl.l1@hotmail.com>
2017-09-26 23:04:22 +1000

    Testing profile modification with v-text-field

--- pages/profile.vue
+++ pages/profile.vue
@@ -3,8 +3,23 @@
     <v-avatar size="36px" tile><img :src="getUser.photoURL" alt="avatar"></v-avatar>
     <h1>{{getUser.displayName}}</h1>
     <h2>{{getUser.email}}</h2>
-    <h2>About Me: {{this.bioText}}</h2>
-    <h3>Age: {{this.age}}</h3>
+<v-card class="secondary elevation-0">
+      <v-card-text>
+        <v-subheader>About Me</v-subheader>
+        <v-container fluid>
+          <v-layout row>
+            <v-flex xs12>
+              <v-text-field
+                box
+                name="input-1"
+                textarea
+                dark
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+    </v-card>    <h3>Age: {{this.age}}</h3>
     <h3>Looking for a {{this.preference}}</h3>
     <v-btn error round @click.native="signOut">Logout</v-btn>
 </div>
@@ -16,7 +31,7 @@ import store from '../store';
     store,
     data: () => ({
         newMessage: '',
-        bioText: 'This is my bio',
+        bioText: 'The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through.',
         age: 'N/A',
         preference: 'Sugar baby'
     }),
