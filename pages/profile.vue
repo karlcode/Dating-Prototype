7e552aab93fee181019b006fe5bb358a567b8d34
karlcode <karl.l1@hotmail.com>
2017-09-27 21:07:22 +1000

    Stylistic changes to profile page and removal of logout button (to be placed in header)

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,28 +1,28 @@
 <template>
-<div class="profile">
+<v-container grid-list-md text-xs-center>
+<v-layout row >
+<v-flex xs12 sm4 >
     <v-avatar size="36px" tile><img :src="getUser.photoURL" alt="avatar"></v-avatar>
-    <h1>{{getUser.displayName}}</h1>
-    <h2>{{getUser.email}}</h2>
-<v-card class="secondary elevation-0">
-      <v-card-text>
-        <v-subheader>About Me</v-subheader>
-        <v-container fluid>
-          <v-layout row>
-            <v-flex xs12>
-              <v-text-field
-                box
-                name="input-1"
-                textarea
-                dark
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-      </v-card-text>
-    </v-card>    <h3>Age: {{this.age}}</h3>
-    <h3>Looking for a {{this.preference}}</h3>
-    <v-btn error round @click.native="signOut">Logout</v-btn>
-</div>
+    <span>{{getUser.displayName}}</span>
+    <p>Age: {{this.age}}</p>
+    <p>{{getUser.email}}</p>
+</v-flex>
+<v-flex xs12 sm8>
+
+
+    <v-text-field
+              name="input-7-3"
+              label="About me"
+              :value="this.bioText"
+              multi-line
+    ></v-text-field>
+    <p>Occupation</p>
+    <p>Looking for a {{this.preference}}</p>
+    
+    </v-flex>
+   
+</v-layout>
+</v-container>
 </template>
 
 <script>
@@ -35,12 +35,6 @@ import store from '../store';
         age: 'N/A',
         preference: 'Sugar baby'
     }),
-    methods: {
-        signOut() {
-                store.dispatch('logoutUser')
-                this.$router.push({path: '/'})
-            }
-    },
     computed: {
         getUser(){
             return store.getters.getUser
