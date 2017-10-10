db8fbc7c6e5288dcc7e1d615d22b45bef6a77ade
karlcode <karl.l1@hotmail.com>
2017-10-10 18:28:34 +1100

    Convert nuxt-link to pass params on path

--- pages/conversation.vue
+++ pages/conversation.vue
@@ -14,7 +14,7 @@
     </v-toolbar>
     <v-list two-line>
       <template v-for="(chat, index) in chats">
-      <nuxt-link to="/conversation/chat">
+      <nuxt-link :to="{ path: '/conversation/chat', params: { chatroom: chat }}">
         <v-list-tile avatar ripple v-bind:key="index">
           <v-list-tile-content>
             <v-list-tile-title>{{ chat.sender }}</v-list-tile-title>
